08ddcc486a269ed24f50fc54271262c3bc942443
zori13 <zori.gon@mail.com>
2023-11-23 16:55:31 +0100

    Ejercicio 3

--- src/ejercicio-03/index.js
+++ src/ejercicio-03/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+async function opAsin(){
+    await new Promise ((resolve)=>setTimeout(resolve,1000))
+    return 'Operación completada'
+}
+async function ejOp(){
+    const resul=await opAsin()
+    console.log(resul)
+
+}
+ejOp()
