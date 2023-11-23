6b8015f166e7ff951eb4309b76ff4b2dd1a79bb8
zori13 <zori.gon@mail.com>
2023-11-23 22:34:32 +0100

    Ejercicio 13

--- src/ejercicio-13/index.js
+++ src/ejercicio-13/index.js
@@ -1,1 +1,11 @@
-// Escribe tu código aquí:
+const axios=require ('axios')
+async function obtenerDatos(){
+    try{
+        const respuesta=await axios.get('https://jsonplaceholder.typicode.com/posts/invalid')
+        console.log(respuesta.data)
+    }
+    catch(error){
+        console.error('Error al obtener datos: ',error.message)
+    }
+}
+obtenerDatos()
