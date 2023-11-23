91a5a1057514e116e8e25aeec2f1cd1cfe8b8177
zori13 <zori.gon@mail.com>
2023-11-23 22:20:09 +0100

    Ejercicio 11

--- src/ejercicio-11/index.js
+++ src/ejercicio-11/index.js
@@ -1,1 +1,9 @@
-// Escribe tu código aquí:
+const p1= new Promise((resolve,reject)=>
+setTimeout(()=>reject('Error en Promesa 1'), 2000))
+const p2= new Promise((resolve)=>
+setTimeout(()=>resolve(2),4000))
+const p3= new Promise((resolve,reject)=>
+setTimeout(()=>reject('Error en Promesa 3'), 1000))
+Promise.allSettled([p1,p2,p3]).then((resultados)=>{
+    console.log('Estado de las Promesas:', resultados)
+})
