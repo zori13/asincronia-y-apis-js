dca6bcf4ef2f468789e70dff0500d62ebaabd944
zori13 <zori.gon@mail.com>
2023-11-23 17:14:09 +0100

    Ejercicio 4

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+const promesaRechazada =new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        reject('Promesa rechazada')
+    },2000)
+})
+promesaRechazada.catch((error)=>{
+    console.log('Error:', error)
+})
